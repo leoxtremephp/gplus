9a448363b6f8edc5f70d0cae8d6e03d2df414342
Leonardo Yanes Batista <leo.xtremephp@gmail.com>
2018-07-09 04:41:40 -0400

    Define User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    /**
+     * Users Table
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +22,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -26,4 +35,42 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    /**
+     * Many to Many Users and Roles
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        return $this->roles()->where(['name' => $role])->first() ? true : false;
+    }
+
+    public function hasAnyRole($roles): bool
+    {
+        switch (gettype($roles)) {
+
+            case 'array':
+                foreach ($roles as $role) {
+                    if ($this->hasRole($role)) {
+                        return true;
+                    }
+                }
+                break;
+
+            case 'string':
+                if ($this->hasRole($roles)) {
+                    return true;
+                }
+                break;
+
+            default:
+                return false;
+        }
+    }
 }
