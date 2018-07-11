3f5f223fd347d76aa953281626f7a0b933467945
Leonardo Yanes Batista <leo.xtremephp@gmail.com>
2018-07-10 23:14:13 -0400

    User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,14 +17,12 @@ class User extends Authenticatable
     protected $table = 'users';
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that aren't mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    protected $guarded = [
+        'id'
     ];
 
     /**
